208f4d57864043bb9d3ddaa176142ff72a4c5224
bostjanhren <bostjanhren6@gmail.com>
2021-04-20 01:45:21 +0200

    narejeno

---
+++ src/Domovi/DomoviProfilnaStran.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Urejanje podatkov ustanove</h1>
+        
+        <br>
+    <form action = "/domacastrandom">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Dodaj telefonsko številko ustanove</label>
+            <input type="tel" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Telefonska številka">
+        </div>
+        <div class="form-group">
+            <label for="address">Spremeni naslov ustanove</label>
+            <input type="text" class="form-control" id="InputAddress" placeholder="Naslov ustanove">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Spremeni geslo</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Opis ustanove</label>
+            <textarea class="form-control" aria-label="With textarea"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Posodobi podatke</button>
+    </form>
+    </div>
+</template>
---
+++ src/Domovi/DomoviTermini.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Urejanje terminov za obsike</h1>
+        
+        <br>
+
+    <div class="d-flex flex-row bd-highlight mb-3">
+        <div class="p-2 bd-highlight">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    Ponedeljek 26.04.2021
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">11-12</li>
+                    <li class="list-group-item">12-13</li>
+                    <li class="list-group-item">15-16</li>
+                </ul>
+            </div>
+        </div>
+        <div class="p-2 bd-highlight">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    Torek 27.04.2021
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">11-12</li>
+                    <li class="list-group-item">12-13</li>
+                    <li class="list-group-item">15-16</li>
+                </ul>
+            </div>
+        </div>
+        
+    </div>
+
+    <form action="/domacastrandom">
+        <button type="submit" class="btn btn-primary">Shrani spremembe</button>
+    </form>
+    </div>
+</template>
