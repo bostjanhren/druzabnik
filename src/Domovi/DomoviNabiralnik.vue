e2d4e7653be7d5e5c408f3f488cfe044a8381c4c
Tadej Kosi <kosi10.tadej@gmail.com>
2021-04-20 10:29:30 +0200

    nev files

---
+++ src/Domovi/DomoviNabiralnik.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Nabiralnik</h1>
+
+        <form action = "/domacastrandom">
+            <button type="submit" class="btn btn-primary">Domov</button>
+        </form>
+
+        <br>
+
+        <p>
+            Prostor kjer nam bo pokazalo tekoče pogovore / vse prej ustvarjene pogovore z vsako osebo posebej.
+        </p>
+
+        <form action = "/instancaSporocila">
+            <button type="submit" class="btn btn-secondary">Chat z osebo xxxxx</button>
+        </form>
+
+    </div>
+</template>
