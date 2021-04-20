5728dac8a18c54f2c22f3c6a7388a4688891f3ee
bostjanhren <bostjanhren6@gmail.com>
2021-04-20 10:33:43 +0200

    popravljen stavek

--- src/Prostovoljci/prostovoljciInfoZaDomPage.vue
+++ src/Prostovoljci/prostovoljciInfoZaDomPage.vue
@@ -15,7 +15,7 @@
             <div class="form-group">
             <button class="btn btn-primary" :disabled="loading">
                 <span class="spinner-border spinner-border-sm" v-show="loading"></span>
-                <span>Podatke o domu in aktivnost</span>
+                <span>Podatki o domu in aktivnostih</span>
             </button>
             </div>
         </form>
