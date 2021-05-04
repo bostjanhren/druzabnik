3b7e27b2e34c1617861ca8892b7d5bb03fd6a507
bostjanhren <bostjanhren6@gmail.com>
2021-05-04 02:17:33 +0200

    prva stran

--- src/home/PrvaStran.vue
+++ src/home/PrvaStran.vue
@@ -1,21 +1,34 @@
 <template>
-    <div>
-        <h1>Druzabnik</h1>
-        <form action="/prostovoljec">
-            <div class="form-group">
-            <button class="btn btn-primary" :disabled="loading">
-                <span class="spinner-border spinner-border-sm" v-show="loading"></span>
-                <span>Prostovoljci</span>
-            </button>
-            </div>
-        </form>
-        <form action="/prijavadom">
-            <div class="form-group">
-            <button class="btn btn-primary" :disabled="loading">
-                <span class="spinner-border spinner-border-sm" v-show="loading"></span>
-                <span>Domovi</span>
-            </button>
-            </div>
-        </form>
+    <div class = "prvaStranStil">
+        
+        <div class = "topbar"></div>
+
+        <div class = "underbar">
+            
+            <p id="descriptionText">Povezujemo prostovoljce z več kot 20 domovi za ostarele</p>
+            
+            <form id = "prvaForm1" action="/prostovoljec">
+                <div class="form-group">
+                <button id="prvaStranButton1" :disabled="loading">
+                    <span class="spinner-border spinner-border-sm" v-show="loading"></span>
+                    <span>Prostovoljci</span>
+                </button>
+                </div>
+            </form>
+            <form id = "prvaForm2" action="/prijavadom">
+                <div class="form-group">
+                <button id="prvaStranButton2" :disabled="loading">
+                    <span class="spinner-border spinner-border-sm" v-show="loading"></span>
+                    <span>Domovi</span>
+                </button>
+                </div>
+            </form>
+        </div>
+
+        <div class = "credits">
+            <p>
+                 Ustvarili študentje FD in FRI
+            </p>
+        </div>
     </div>
 </template>
