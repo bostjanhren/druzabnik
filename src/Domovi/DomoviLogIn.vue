9e1c5ac152e476c03b5fc351278cc9f7475cc5ca
Tadej Kosi <kosi10.tadej@gmail.com>
2021-05-05 14:54:32 +0200

    Urejanje prve strani in loginDomovi (CSS)

--- src/Domovi/DomoviLogIn.vue
+++ src/Domovi/DomoviLogIn.vue
@@ -1,19 +1,29 @@
 <template>
-<div>
-    <form action = "/domacastrandom">
-        <div class="form-group">
-            <label for="exampleInputEmail1">E-poštni naslov</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Vnesi email">
+    <div class = "loginDomovi">
+
+        <div class = "topbar"></div>
+
+        <div class = "underbar"> 
+
+            <form action = "/domacastrandom">
+                <div class="E-mail">
+                    <label class= "email-text" for="exampleInputEmail1">E-poštni naslov</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Vnesi email">
+                </div>
+                <div class="Geslo">
+                    <label class= "geslo-text" for="exampleInputPassword1">Geslo</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Geslo">
+                </div>
+                <button id="btnPrijava" type="submit">Prijava</button>
+            </form>
+            <br>
+            <form action = "/registracijadom">
+                <label class= "registracija-text" for="exampleInputPassword1">Ali še niste uporabnik?</label>
+                <button id="btnRegistracija" type="submit">Registracija</button>
+            </form>
+            <form action = "/">
+                <button id="btnNazaj" type="submit">Nazaj</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Geslo</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <button type="submit" class="btn btn-primary">Prijava</button>
-    </form>
-    <br>
-    <form action = "/registracijadom">
-        <button type="submit" class="btn btn-primary">Registracija</button>
-    </form>
-</div>
+    </div>
 </template>
