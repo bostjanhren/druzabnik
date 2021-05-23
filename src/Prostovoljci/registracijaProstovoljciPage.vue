663b92f8613965b3b9dd09f56f7b7af105ca25f1
Atanasova <ea5105@student.uni-lj.si>
2021-05-23 04:27:55 +0200

    zaceli rihtat tako kot hocejo dizajnerje- registrationPageProstovoljci

--- src/Prostovoljci/registracijaProstovoljciPage.vue
+++ src/Prostovoljci/registracijaProstovoljciPage.vue
@@ -2,30 +2,20 @@
   <div class="registrationPageProstovoljci">
     <div class = "topbar"></div>
     <div class = "underbar">
-      <label class = "registracijaProstovoljci1"> Registracija Prostovoljca </label>
+      <label class = "registracijaProstovoljci1">REGISTRACIJA</label>
       <form @submit.prevent="handleSubmit">
         <div class = "UporabniskoIme">
-          <label class = "uporabniskoIme-text" for = "exampleUporabniskoIme"> Uporabnisko Ime </label>
-          <input type = "uporabniskoIme" v-model="username" class = "form-control" id = "exampleUporabniskoIme1" aria-describedby="uporabniskoImeHelp" placeholder="Uporabnisko ime">
+          <input type = "uporabniskoIme" v-model="username" id = "exampleUporabniskoIme1" aria-describedby="uporabniskoImeHelp" placeholder="Uporabnisko ime">
         </div>
         <div class = "Ime">
-          <label class = "Ime-text" for = "exampleIme">  Ime </label>
-          <input type = "Ime1" v-model="firstName" class = "form-control" id = "exampleIme1" aria-describedby="ImeHelp" placeholder="Ime">
-        </div>
-        <div class = "Priimek">
-          <label class = "Priimek-text" for = "exampleIme">  Priimek </label>
-          <input type = "Priimek1" v-model="lastName" class = "form-control" id = "examplePriimek1" aria-describedby="PriimekHelp" placeholder="Primek">
+          <input type = "Ime1" v-model="firstName" id = "exampleIme1" aria-describedby="ImeHelp" placeholder="e-naslov">
         </div>
         <div class="GesloProstovoljci">
-          <label class= "GesloProstovoljci-text" for="exampleInputPassword1">Geslo</label>
-          <input type="GesloProstovoljci1" v-model="password" class="form-control" id="exampleInputPassword1" placeholder="Geslo">
+          <input type="GesloProstovoljci1" v-model="password" id="exampleGesloProstovoljci1" placeholder="Geslo">
         </div>
-        <button id="btnRegistracijaProstovoljci" type="submit">Registracija</button>
+        <button id="btnRegistracijaProstovoljci" type="submit">Nadaljuj</button>
       </form>
       <br>
-      <form action = "/">
-        <button id="btnNazaj3" type="submit">Nazaj</button>
-      </form>
     </div>
   </div>
 </template>
