cc3e6bb4529749e7afafef57ea55b59536f9a2b8
Atanasova <ea5105@student.uni-lj.si>
2021-05-24 10:08:04 +0200

    Koncane strani: prvaStran, prostovoljci: login/signup, domovi: login/signup za iPhone 6/7/8 in dodano potreben font Inter

--- src/Domovi/DomoviRegistracija.vue
+++ src/Domovi/DomoviRegistracija.vue
@@ -2,37 +2,24 @@
   <div class = "registrationPageDomove">
     <div class = "topbar"></div>
     <div class = "underbar">
-        <label class = "registracijaDom"> Registracija Doma </label>
-        <label class = "registracijaDom-tekst"> Prosim vnesite podatke vaše ustanove </label>
+        <label class = "registracijaDom">REGISTRACIJA</label>
         <form>
             <!-- ime doma upokojencev -->
             <div class = "registacijaImeDom">
-                 <label class = "registracijaImeDom1-text" for = "exampleRegistracijaImeDom"> Ime dom </label>
-                 <input type = "registracijaImeDom1" class = "form-control" id = "exampleRegistracijaImeDom1" aria-describedby="registracijaImeDomHelp" placeholder="Ime Dom">
-            </div>
-
-            <!--naslov doma upokojencev -->
-            <div class = "registracijaNaslovDom">
-                 <label class = "registracijaNaslovDom1-text" for = "exampleRegistracijaNaslovDom"> Naslov dom </label>
-                 <input type = "registracijaNaslovDom1" class = "form-control" id = "exampleRegistracijaNaslovDom1" aria-describedby="registracijaNaslovDomHelp" placeholder="Naslov">
+                 <input type = "registracijaImeDom1" id = "exampleRegistracijaImeDom1" aria-describedby="registracijaImeDomHelp" placeholder=" Uporabniško ime">
             </div>
 
            <!--E-postni naslov doma upokojencev -->
             <div class = "registracijaPostniNaslovDom">
-                 <label class = "registracijaPostniNaslovDom1-text" for = "exampleRegistracijaPostniNaslovDom"> E-poštni naslov dom </label>
-                 <input type = "registracijaPostniNaslovDom1" class = "form-control" id = "exampleRegistracijaPostniNaslovDom1" aria-describedby="registracijaPostniNaslovDomHelp" placeholder="E-poštni naslov">
+                 <input type = "registracijaPostniNaslovDom1" id = "exampleRegistracijaPostniNaslovDom1" aria-describedby="registracijaPostniNaslovDomHelp" placeholder=" e-naslov">
             </div>
 
             <!--geslo doma upokojencev -->
             <div class = "registracijaGesloDom">
-                 <label class = "registracijaGesloDom1-text" for = "exampleRegistracijaGesloDom"> Geslo </label>
-                 <input type = "registracijaGesloDom1" class = "form-control" id = "exampleRegistracijaGesloDom1" aria-describedby="registracijaGesloDomHelp" placeholder="Geslo">
+                 <input type = "registracijaGesloDom1" id = "exampleRegistracijaGesloDom1" aria-describedby="registracijaGesloDomHelp" placeholder=" Geslo">
             </div>
-            <button id="btnRegistracijaDom" type="submit">Registracija</button>
-        </form>
-        <br>
-         <form action = "/">
-            <button id="btnNazaj4" type="submit">Nazaj</button>
+             <label class = "pogojiUporabe">S klikom gumba "Nadaljuj" se uporabnik strinja s pogoji uporabe.</label>
+            <button id="btnRegistracijaDom" type="submit">Nadaljuj</button>
         </form>
     </div>
   </div>
--- src/Prostovoljci/registracijaProstovoljciPage.vue
+++ src/Prostovoljci/registracijaProstovoljciPage.vue
@@ -5,14 +5,15 @@
       <label class = "registracijaProstovoljci1">REGISTRACIJA</label>
       <form @submit.prevent="handleSubmit">
         <div class = "UporabniskoIme">
-          <input type = "uporabniskoIme" v-model="username" id = "exampleUporabniskoIme1" aria-describedby="uporabniskoImeHelp" placeholder="Uporabnisko ime">
+          <input type = "uporabniskoIme" v-model="username" id = "exampleUporabniskoIme1" aria-describedby="uporabniskoImeHelp" placeholder=" Uporabnisko ime">
         </div>
         <div class = "Ime">
-          <input type = "Ime1" v-model="firstName" id = "exampleIme1" aria-describedby="ImeHelp" placeholder="e-naslov">
+          <input type = "Ime1" v-model="firstName" id = "exampleIme1" aria-describedby="ImeHelp" placeholder=" e-naslov">
         </div>
         <div class="GesloProstovoljci">
-          <input type="GesloProstovoljci1" v-model="password" id="exampleGesloProstovoljci1" placeholder="Geslo">
+          <input type="GesloProstovoljci1" v-model="password" id="exampleGesloProstovoljci1" placeholder=" Geslo">
         </div>
+        <label class = "pogojiUporabe">S klikom gumba "Nadaljuj" se uporabnik strinja s pogoji uporabe.</label>
         <button id="btnRegistracijaProstovoljci" type="submit">Nadaljuj</button>
       </form>
       <br>
